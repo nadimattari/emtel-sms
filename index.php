9f7a46b7657b2819258c787ab0cf5ba9cc0f2151
Nadim Attari <nadim.attari@gmail.com>
2024-03-08 01:42:08 +0400

    Ciphers = no security

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (!file_exists($ini_file)) {
 $INI    = parse_ini_file($ini_file);
 $phone  = filter_input(INPUT_GET, 'phone', FILTER_SANITIZE_NUMBER_INT);
 
-$message = 'Emtel SMS test from AWS. ' . date('Y-m-d H:i:s');
+$message = 'Emtel SMS test on AWS. ' . date('Y-m-d H:i:s') . ' ~nadim';
 $params  = [
     'username' => $INI['EMTEL_USER'],
     'password' => $INI['EMTEL_PASS'],
@@ -21,9 +21,20 @@ $params  = [
 $url = $INI['EMTEL_SEND_SMS_API_ENDPOINT'] . '?' . http_build_query($params);
 
 try {
+    /**
+     * $ curl -k
+     *      --request GET
+     *      --url '$url'
+     *      --ciphers 'DEFAULT@SECLEVEL=0'
+     */
     $client   = new \GuzzleHttp\Client();
-    $response = $client->request('GET', $url);
-
+    $response = $client->request('GET', $url, [
+        'verify' => false,
+        'curl'   => [
+            CURLOPT_SSL_CIPHER_LIST => 'DEFAULT@SECLEVEL=0',
+        ]
+    ]);
+    
     echo json_encode([
         'message-sent'  => $message,
         'to-phone'      => $phone,
@@ -34,10 +45,15 @@ try {
     ], JSON_PRETTY_PRINT);
 }
 catch (Exception $e) {
+    $msg = str_replace(
+        [$INI['EMTEL_USER'], $INI['EMTEL_PASS']], 
+        ['[USER]', '[PASSWD]'], 
+        $e->getMessage()
+    );
     echo json_encode([
         'message-sent' => $message,
         'to-phone'     => $phone,
         'error'        => true,
-        'error-msg'    => $e->getMessage(),
+        'error-msg'    => $msg,
     ], JSON_PRETTY_PRINT);
 }
